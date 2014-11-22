0d8f709983cd382bdf24a1acc68925a57bcb9184
Oleg Ivanov <morhekil@morhekil.net>
2014-11-23 01:24:54 +1100

    json emitter with id hydration

--- feeder/data/data.go
+++ feeder/data/data.go
@@ -1,10 +1,22 @@
 package data
 
-import "time"
+import (
+	"crypto/md5"
+	"fmt"
+	"time"
+)
 
+// Event data respresentation
 type Event struct {
+	ID        string
 	User      string
 	Domain    string
 	Action    string
 	Timestamp time.Time
+}
+
+// Hydrate populates event record with additional derived data
+func (e *Event) Hydrate() {
+	s := fmt.Sprintf("%s-%s-%s", e.User, e.Action, e.Timestamp)
+	e.ID = fmt.Sprintf("%x", md5.Sum([]byte(s)))
 }
